e6d5af330e37480c07bc2a1effafb6ec9518026c
Luketto-dev <luca.francesconi1995@gmail.com>
2022-07-12 17:12:12 +0200

    milenstone1 completata

---
+++ TheHeader.php
@@ -0,0 +1,6 @@
+
+<header class="py-2 px-3 d-flex">
+    <div class="header-logo">
+        <img src="../img/logo-small.svg" alt="">
+    </div>
+</header>
---
+++ milenston1/index_php.php
@@ -0,0 +1,52 @@
+<?php
+require_once "../db.php";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <?php require "../TheHeader.php"?>
+    <main>
+        <div class="container">
+            <div class="py-5">
+                <div class="row row-cols-5 gy-3 gx-2">
+                    <?php foreach($resp as $disco){?>
+                    <div class="col flex-shrink-0">
+                        <div class="disc text-center">
+                            <div class="disc-poster">
+                                <img class="img-fluid" src="<?php echo $disco["poster"] ?>" alt="infoDisc.title">
+                            </div>
+                            <div class="disc-title">
+                                <h4 class="text-white pt-2"><?php echo $disco["title"]?></h4>
+                            </div>
+                            <div class="disc-author text-white-50">
+                            <?php echo $disco["author"]?>
+                            </div>
+                            <div class="disc-year text-white-50">
+                            <?php echo $disco["year"]?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php }?>
+                </div>
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
+
