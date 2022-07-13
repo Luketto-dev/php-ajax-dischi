b9d69407428df611b92d29c2597cb927306c5cd8
Luketto-dev <luca.francesconi1995@gmail.com>
2022-07-13 15:39:44 +0200

    implementato filtro per genere

--- milenstone2/api/dischi.php
+++ milenstone2/api/dischi.php
@@ -3,5 +3,17 @@
 
     header("Content-Type: application/json");
 
-    echo json_encode($resp)
+    echo json_encode($resp);
+
+    $filtroGenere = key_exists("genere", $_GET) ? $_GET["genere"] : "";
+
+    var_dump($_GET);
+
+    $datiFiltrati = $dati;
+
+    if ($filtroCottura) {
+        $datiFiltrati = array_filter($datiFiltrati, function($disco) use ($filtroGenere) {
+            return $disco["genre"] === $filtroGenere;
+        });
+    }
 ?>
